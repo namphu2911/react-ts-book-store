754d30b27fb465a6b567e7016744e19de26243ec
namphu2911 <kuboy151@gmail.com>
2025-02-14 00:27:37 +0700

    update: setup layout, tsconfigPaths

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Layout from './layout'
+import Layout from '@/layout'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import BookPage from 'pages/client/book';
+import AboutPage from 'pages/client/about';
+import LoginPage from 'pages/client/auth/login';
+import RegisterPage from 'pages/client/auth/register';
+import 'styles/global.scss'
 
 const router = createBrowserRouter([
   {
@@ -10,21 +15,21 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/book",
-        element: <div>Book Page</div>,
+        element: <BookPage />,
       },
       {
         path: "/about",
-        element: <div>About Page</div>,
+        element: <AboutPage />,
       }
     ]
   },
   {
     path: "/login",
-    element: <div>Login Page</div>,
+    element: <LoginPage />,
   },
   {
     path: "/register",
-    element: <div>Register Page</div>,
+    element: <RegisterPage />,
   },
 ]);
 
